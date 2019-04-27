29654e56cd47141f85f743e006dcdb665ea86102
switzerb <brenna.switzer@gmail.com>
2019-04-26 19:21:08 -0700

    Play with BindingResult class

--- src/main/java/com/brennaswitzer/cookbook/web/RecipeController.java
+++ src/main/java/com/brennaswitzer/cookbook/web/RecipeController.java
@@ -5,10 +5,13 @@ import com.brennaswitzer.cookbook.services.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/recipe")
@@ -18,7 +21,12 @@ public class RecipeController {
     private RecipeService recipeService;
 
     @PostMapping("")
-    public ResponseEntity<Recipe> createNewRecipe(@RequestBody Recipe recipe) {
+    public ResponseEntity<?> createNewRecipe(@Valid @RequestBody Recipe recipe, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return new ResponseEntity<String>("Invalid Recipe", HttpStatus.BAD_REQUEST);
+        }
+
         Recipe recipe1 = recipeService.saveOrUpdateRecipe(recipe);
         return new ResponseEntity<>(recipe, HttpStatus.CREATED);
     }
