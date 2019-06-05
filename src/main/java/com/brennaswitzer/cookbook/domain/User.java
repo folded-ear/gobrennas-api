50823d76a8c0505d76715104b8c42b398f2f7cb5
switzerb <brenna.switzer@gmail.com>
2019-06-04 21:26:48 -0700

    Adds user domain model

--- src/main/java/com/brennaswitzer/cookbook/domain/User.java
+++ src/main/java/com/brennaswitzer/cookbook/domain/User.java
@@ -12,14 +12,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String first;
-    private String username;
     private String email;
+    private String password;
 
     public User() {}
 
     public User(String first, String username, String email, String password) {
         this.first = first;
-        this.username = username;
         this.email = email;
     }
 
@@ -39,14 +38,6 @@ public class User {
         this.first = first;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -54,4 +45,13 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
