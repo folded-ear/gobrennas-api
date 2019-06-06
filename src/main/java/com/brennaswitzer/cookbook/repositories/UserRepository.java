adf1ba7e336a5cc4ac8dc53d51fc362a5fb2b669
switzerb <brenna.switzer@gmail.com>
2019-06-05 20:33:19 -0700

    Adds application env and user respository

---
+++ src/main/java/com/brennaswitzer/cookbook/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.brennaswitzer.cookbook.repositories;
+
+import com.brennaswitzer.cookbook.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
