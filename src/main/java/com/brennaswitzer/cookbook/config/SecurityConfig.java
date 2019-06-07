430843891d94576ad99db19d113cf869bc43fa7b
switzerb <brenna.switzer@gmail.com>
2019-06-07 16:42:44 -0700

    Sorts the client-side flow of authentication

--- src/main/java/com/brennaswitzer/cookbook/config/SecurityConfig.java
+++ src/main/java/com/brennaswitzer/cookbook/config/SecurityConfig.java
@@ -19,6 +19,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 /**
