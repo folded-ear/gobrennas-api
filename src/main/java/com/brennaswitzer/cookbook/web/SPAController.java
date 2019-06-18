52b7348ac08ad6d03330792e51f666a891a62a8b
Barney Boisvert <bboisvert@gmail.com>
2019-06-18 16:39:10 -0700

    SPAController extends BasicErrorController, and is _much_ simpler

--- src/main/java/com/brennaswitzer/cookbook/web/SPAController.java
+++ src/main/java/com/brennaswitzer/cookbook/web/SPAController.java
@@ -1,89 +1,39 @@
 package com.brennaswitzer.cookbook.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.nio.charset.Charset;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
-@Controller
-public class SPAController implements ErrorController {
-    @Value("${spa.default-resource}")
-    private String defaultResource;
+public class SPAController extends BasicErrorController {
 
-    private final ObjectMapper objectMapper;
-
-    public SPAController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
+    public SPAController(ErrorAttributes errorAttributes, ErrorProperties errorProperties) {
+        super(errorAttributes, errorProperties);
     }
 
-    private class ErrorBody {
-        private int status = 500;
-        private String message = "Something bad happened.";
-
-        private ErrorBody() {}
-        private ErrorBody(int status) {
-            this.status = status;
-        }
-        private ErrorBody(String message) {
-            this.message = message;
-        }
-        private ErrorBody(int status, String message) {
-            this.status = status;
-            this.message = message;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
-
-    @RequestMapping("/error")
-    public ResponseEntity<String> handleError(HttpServletRequest request) throws IOException {
-        Object message = request.getAttribute(RequestDispatcher.ERROR_MESSAGE);
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        ErrorBody body;
-        if (status != null) {
-            int statusCode = Integer.valueOf(status.toString());
-
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return ResponseEntity.ok()
-                        .contentType(MediaType.TEXT_HTML)
-                        .body(
-                                StreamUtils.copyToString(
-                                        getClass().getResourceAsStream(defaultResource),
-                                        Charset.defaultCharset()));
-            }
-            body = new ErrorBody(statusCode);
-        } else {
-            body = new ErrorBody();
-        }
-        if (message != null) {
-            body.message = message.toString();
-        }
-        return ResponseEntity.status(body.status)
-                .contentType(MediaType.APPLICATION_JSON)
-                    .body(objectMapper.writeValueAsString(
-                            body));
+    public SPAController(ErrorAttributes errorAttributes, ErrorProperties errorProperties, List<ErrorViewResolver> errorViewResolvers) {
+        super(errorAttributes, errorProperties, errorViewResolvers);
     }
 
     @Override
-    public String getErrorPath() {
-        return "/error";
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        HttpStatus status = getStatus(request);
+        if (HttpStatus.NOT_FOUND.equals(status)) {
+            response.setStatus(HttpStatus.OK.value());
+            request.removeAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+            request.removeAttribute(RequestDispatcher.ERROR_EXCEPTION_TYPE);
+            request.removeAttribute(RequestDispatcher.ERROR_EXCEPTION);
+            request.removeAttribute(RequestDispatcher.ERROR_MESSAGE);
+            return new ModelAndView("forward:/");
+        }
+        return super.errorHtml(request, response);
     }
 
 }
