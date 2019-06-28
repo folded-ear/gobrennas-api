b8221c236c1eacdec6768be9cbc4ad474cf08e51
Barney Boisvert <bboisvert@gmail.com>
2019-06-28 12:46:42 -0700

    use `/api/user/me` for fetching profile, not just `/user/me`

--- src/main/java/com/brennaswitzer/cookbook/web/FriendController.java
+++ src/main/java/com/brennaswitzer/cookbook/web/FriendController.java
@@ -5,11 +5,9 @@ import com.brennaswitzer.cookbook.repositories.UserRepository;
 import com.brennaswitzer.cookbook.security.CurrentUser;
 import com.brennaswitzer.cookbook.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
@@ -22,7 +20,6 @@ public class FriendController {
     private UserRepository userRepository;
 
     @GetMapping("")
-    @ResponseStatus(HttpStatus.OK)
     public Iterable<User> getFriends(
             @CurrentUser UserPrincipal userPrincipal
     ) {
--- src/main/java/com/brennaswitzer/cookbook/web/UserController.java
+++ src/main/java/com/brennaswitzer/cookbook/web/UserController.java
@@ -1,24 +1,25 @@
 package com.brennaswitzer.cookbook.web;
 
 import com.brennaswitzer.cookbook.domain.User;
-import com.brennaswitzer.cookbook.exceptions.ResourceNotFoundException;
 import com.brennaswitzer.cookbook.repositories.UserRepository;
 import com.brennaswitzer.cookbook.security.CurrentUser;
 import com.brennaswitzer.cookbook.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
 @RestController
+@RequestMapping("api/user")
+@PreAuthorize("hasRole('USER')")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user/me")
-    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/me")
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.getById(userPrincipal.getId());
     }
