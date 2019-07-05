4400f7275d63b2da0662fafa4869780e942840c3
Barney Boisvert <bboisvert@gmail.com>
2019-07-05 09:14:41 -0700

    populate `rawIngredients` on RecipeBox's contents

--- src/test/java/com/brennaswitzer/cookbook/util/RecipeBox.java
+++ src/test/java/com/brennaswitzer/cookbook/util/RecipeBox.java
@@ -1,5 +1,6 @@
 package com.brennaswitzer.cookbook.util;
 
+import com.brennaswitzer.cookbook.domain.IngredientRef;
 import com.brennaswitzer.cookbook.domain.PantryItem;
 import com.brennaswitzer.cookbook.domain.Recipe;
 
@@ -15,22 +16,34 @@ public final class RecipeBox {
         FRIED_CHICKEN = new Recipe("Fried Chicken");
         FRIED_CHICKEN.addIngredient("2", new PantryItem("egg"), "shelled");
         FRIED_CHICKEN.addIngredient(new PantryItem("chicken"), "deboned");
+        setRawIngredients(FRIED_CHICKEN);
 
         PIZZA_SAUCE = new Recipe("Pizza Sauce");
         PIZZA_SAUCE.addIngredient("1 lbs", new PantryItem("fresh tomatoes"), "seeded and crushed");
         PIZZA_SAUCE.addIngredient("1 (6 oz) can", new PantryItem("tomato paste"));
         PIZZA_SAUCE.addIngredient(new PantryItem("italian seasoning"));
+        setRawIngredients(PIZZA_SAUCE);
 
         PIZZA_CRUST = new Recipe("Pizza Crust");
         PIZZA_CRUST.addIngredient("2 c", new PantryItem("flour"));
         PIZZA_CRUST.addIngredient("1 c", new PantryItem("water"));
         PIZZA_CRUST.addIngredient("1 packet", new PantryItem("yeast"));
         PIZZA_CRUST.addIngredient("1 Tbsp", new PantryItem("sugar"));
+        setRawIngredients(PIZZA_CRUST);
 
         PIZZA = new Recipe("Pizza");
         PIZZA.addIngredient("4 oz", new PantryItem("pepperoni"));
         PIZZA.addIngredient("8 oz", PIZZA_SAUCE);
         PIZZA.addIngredient("1", PIZZA_CRUST);
+        setRawIngredients(PIZZA);
+    }
+
+    private static void setRawIngredients(Recipe r) {
+        StringBuilder sb = new StringBuilder();
+        for (IngredientRef ref : r.getIngredients()) {
+            sb.append(ref).append('\n');
+        }
+        r.setRawIngredients(sb.toString());
     }
 
 }
