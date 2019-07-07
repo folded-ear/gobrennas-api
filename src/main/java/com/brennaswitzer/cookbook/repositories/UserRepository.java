91c7225bfd11f0340c0a6df6e42f44430a7e3e42
Barney Boisvert <bboisvert@gmail.com>
2019-07-07 14:59:26 -0700

    return "friends" as UserInfo, instead of User

--- src/main/java/com/brennaswitzer/cookbook/repositories/UserRepository.java
+++ src/main/java/com/brennaswitzer/cookbook/repositories/UserRepository.java
@@ -1,13 +1,14 @@
 package com.brennaswitzer.cookbook.repositories;
 
 import com.brennaswitzer.cookbook.domain.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 
     User getByName(String name);
@@ -16,7 +17,7 @@ public interface UserRepository extends CrudRepository<User,Long> {
 
     User getById(Long id);
 
-    // kludge for FriendController's pretent implementation
-    Iterable<User> findByIdNot(Long idToExclude);
+    // kludge for FriendController's pretend implementation
+    List<User> findByIdNot(Long idToExclude);
 
 }
