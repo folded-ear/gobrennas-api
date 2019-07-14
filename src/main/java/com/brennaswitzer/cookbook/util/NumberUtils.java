fbd8176a3975fa8ffe70562f9bb26e9c529ffd06
Barney Boisvert <bboisvert@gmail.com>
2019-07-14 15:04:17 -0700

    fail even MOAR aggressively on bad quantities

--- src/main/java/com/brennaswitzer/cookbook/util/NumberUtils.java
+++ src/main/java/com/brennaswitzer/cookbook/util/NumberUtils.java
@@ -2,9 +2,7 @@ package com.brennaswitzer.cookbook.util;
 
 import com.brennaswitzer.cookbook.antlr.FloatLexer;
 import com.brennaswitzer.cookbook.antlr.FloatParser;
-import org.antlr.v4.runtime.BailErrorStrategy;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -16,16 +14,30 @@ public final class NumberUtils {
 
     public static Float parseFloat(String str) {
         if (str == null) return null;
+        str = str.trim();
+        if (str.isEmpty()) return null;
         try {
             FloatLexer lexer = new FloatLexer(CharStreams.fromString(str.toLowerCase()));
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             FloatParser parser = new FloatParser(tokens);
             parser.setErrorHandler(new BailErrorStrategy());
+            parser.addErrorListener(new BaseErrorListener() {
+                @Override
+                public void syntaxError(
+                        Recognizer<?, ?> recognizer,
+                        Object offendingSymbol,
+                        int line,
+                        int charPositionInLine,
+                        String msg, RecognitionException e
+                ) {
+                    // any syntax error is immediate failure; we're boolean here
+                    throw e;
+                }
+            });
             FloatParser.StartContext tree = parser.start();
-            if (parser.getNumberOfSyntaxErrors() > 0) return null;
             return Math.round(tree.val * 1000) / 1000f;
         } catch (Exception e) {
-            logger.error("Failed to parseFloat(" + str + ")", e);
+            logger.error("Failed to parseFloat(\"" + str + "\")", e);
             return null;
         }
     }
