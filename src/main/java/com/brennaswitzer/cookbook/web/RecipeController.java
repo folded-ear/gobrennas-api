0c6da7c4ca64942346cf71699a2b4de80c9b5465
Barney Boisvert <bboisvert@gmail.com>
2019-07-16 18:24:22 -0700

    allow GETting arbitrary Ingredients by id

--- src/main/java/com/brennaswitzer/cookbook/web/RecipeController.java
+++ src/main/java/com/brennaswitzer/cookbook/web/RecipeController.java
@@ -1,5 +1,6 @@
 package com.brennaswitzer.cookbook.web;
 
+import com.brennaswitzer.cookbook.domain.Ingredient;
 import com.brennaswitzer.cookbook.domain.Recipe;
 import com.brennaswitzer.cookbook.payload.IngredientInfo;
 import com.brennaswitzer.cookbook.payload.RecipeAction;
@@ -11,8 +12,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.validation.Valid;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -59,6 +62,35 @@ public class RecipeController {
         recipe.orElseThrow(NoResultException::new);
         return IngredientInfo.from(recipe.get());
     }
+
+    // begin kludge
+    @Autowired private EntityManager em;
+    @SuppressWarnings("JavaReflectionMemberAccess")
+    @GetMapping("/or-ingredient/{id}")
+    public IngredientInfo getIngredientById(@PathVariable("id") Long id) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Ingredient i = em.find(Ingredient.class, id);
+
+        // dynamic dispatch sure would be nice!
+//        IngredientInfo.from(i);
+
+        // a Visitor is a tad heavy for a throwaway kludge...
+
+        // Java doesn't let you switch except on primitives...
+//        switch (i.getClass().getSimpleName()) {
+//            case "PantryItem":
+//                return IngredientInfo.from((PantryItem) i);
+//            case "Recipe":
+//                return IngredientInfo.from((Recipe) i);
+//            default:
+//                throw new IllegalArgumentException("Can't deal with " + i.getClass().getSimpleName() + ". Yet!");
+//        }
+
+        // just reflect it. Screw. That. Poop.
+        return (IngredientInfo) IngredientInfo.class
+                .getMethod("from", i.getClass())
+                .invoke(null, i);
+    }
+    // end kludge
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteRecipe(@PathVariable Long id) {
