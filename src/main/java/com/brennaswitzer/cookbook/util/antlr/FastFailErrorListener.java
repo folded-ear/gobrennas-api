f1d97b77a42fd8df5329be81a62c1c360e5ad081
Barney Boisvert <bboisvert@gmail.com>
2019-08-09 14:38:17 -0700

    NumberUtils can do pure number parsing, or find one in a larger string

---
+++ src/main/java/com/brennaswitzer/cookbook/util/antlr/FastFailErrorListener.java
@@ -0,0 +1,22 @@
+package com.brennaswitzer.cookbook.util.antlr;
+
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+
+public class FastFailErrorListener extends BaseErrorListener {
+
+    @Override
+    public void syntaxError(
+            Recognizer<?, ?> recognizer,
+            Object offendingSymbol,
+            int line,
+            int charPositionInLine,
+            String msg,
+            RecognitionException e
+    ) {
+        // any syntax error is immediate failure; we're boolean here
+        throw e;
+    }
+
+}
