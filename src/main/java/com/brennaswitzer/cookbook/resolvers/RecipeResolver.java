0cd39c7a19da7f9765d113b7bd9837b23327efe3
Barney Boisvert <bboisvert@gmail.com>
2023-03-12 11:24:03 -0700

    use List<String> for recipe labels
    The REST API treats labels as a list of strings, not full objects, so do
    the same over GraphQL (at least for the moment).

--- src/main/java/com/brennaswitzer/cookbook/resolvers/RecipeResolver.java
+++ src/main/java/com/brennaswitzer/cookbook/resolvers/RecipeResolver.java
@@ -2,10 +2,15 @@ package com.brennaswitzer.cookbook.resolvers;
 
 import com.brennaswitzer.cookbook.domain.FavoriteType;
 import com.brennaswitzer.cookbook.domain.Recipe;
+import com.brennaswitzer.cookbook.mapper.LabelMapper;
 import com.brennaswitzer.cookbook.services.favorites.FetchFavorites;
 import graphql.kickstart.tools.GraphQLResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @SuppressWarnings("unused") // component-scanned for graphql-java
 @Component
@@ -13,6 +18,16 @@ public class RecipeResolver implements GraphQLResolver<Recipe> {
 
     @Autowired
     private FetchFavorites fetchFavorites;
+
+    @Autowired
+    private LabelMapper labelMapper;
+
+    public List<String> getLabels(Recipe recipe) {
+        return recipe.getLabels()
+                .stream()
+                .map(labelMapper::labelToString)
+                .collect(toList());
+    }
 
     public boolean isFavorite(Recipe recipe) {
         return fetchFavorites.byObject(
