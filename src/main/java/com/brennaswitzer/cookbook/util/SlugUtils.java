29183324f343ea1d44b0de37e6d1c4853f65ef67
Barney Boisvert <bboisvert@gmail.com>
2023-08-13 10:21:50 -0700

    endpoint for getting plan share info (like recipe)

--- src/main/java/com/brennaswitzer/cookbook/util/SlugUtils.java
+++ src/main/java/com/brennaswitzer/cookbook/util/SlugUtils.java
@@ -1,17 +1,25 @@
 package com.brennaswitzer.cookbook.util;
 
-import org.springframework.util.Assert;
-
 public class SlugUtils {
 
     public static String toSlug(String name) {
-        Assert.notNull(name, "Null cannot be converted to a slug");
-        if (name.length() == 0) return name;
-        return name.substring(0, 1).toLowerCase() +
+        return toSlug(name, -1);
+    }
+
+    public static String toSlug(String name, int maxLength) {
+        if (name == null || name.isBlank()) return "empty";
+        String slug = name.substring(0, 1).toLowerCase() +
                 name.substring(1)
-                .replaceAll("([A-Z0-9]+)", "-$1")
-                .replaceAll("[^a-zA-Z0-9]+", "-")
-                .toLowerCase();
+                        .replaceAll("([A-Z0-9]+)", "-$1")
+                        .replaceAll("[^a-zA-Z0-9]+", "-")
+                        .toLowerCase();
+        if (maxLength > 0 && slug.length() > maxLength) {
+            slug = slug.substring(0, maxLength);
+            while (slug.endsWith("-")) {
+                slug = slug.substring(0, slug.length() - 1);
+            }
+        }
+        return slug;
     }
 
 }
--- src/test/java/com/brennaswitzer/cookbook/util/SlugUtilsTest.java
+++ src/test/java/com/brennaswitzer/cookbook/util/SlugUtilsTest.java
@@ -3,24 +3,29 @@ package com.brennaswitzer.cookbook.util;
 import org.junit.jupiter.api.Test;
 
 import static com.brennaswitzer.cookbook.util.SlugUtils.toSlug;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SlugUtilsTest {
 
     @Test
     public void happy() {
-        assert "".equals(toSlug(""));
-        assert "a".equals(toSlug("a"));
-        assert "a".equals(toSlug("A"));
-        assert "cat-dog".equals(toSlug("catDog"));
-        assert "cat-dog".equals(toSlug("CatDog"));
-        assert "team-usa".equals(toSlug("TeamUSA"));
+        assertEquals("empty", toSlug(null));
+        assertEquals("empty", toSlug(""));
+        assertEquals("empty", toSlug("   \t\t \n  "));
+        assertEquals("a", toSlug("a"));
+        assertEquals("a", toSlug("A"));
+        assertEquals("cat-dog", toSlug("catDog"));
+        assertEquals("cat-dog", toSlug("CatDog"));
+        assertEquals("team-usa", toSlug("TeamUSA"));
     }
 
     @Test
-    public void refuseNull() {
-        assertThrows(IllegalArgumentException.class, () ->
-                toSlug(null));
+    void truncation() {
+        assertEquals("team-usa", toSlug("TeamUSA", 10));
+        assertEquals("team-usa", toSlug("TeamUSA", 8));
+        assertEquals("team-u", toSlug("TeamUSA", 6));
+        // no trailing dash!
+        assertEquals("team", toSlug("TeamUSA", 5));
     }
 
 }
