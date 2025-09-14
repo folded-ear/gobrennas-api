c20a036d3ec735cc171bfa99c6510dc566bb11b6
Barney Boisvert <bboisvert@gmail.com>
2025-09-14 09:59:23 -0700

    remove UnexpectedRollbackException stuff from the txn execution strat

--- src/main/java/com/brennaswitzer/cookbook/config/GraphQLConfig.java
+++ src/main/java/com/brennaswitzer/cookbook/config/GraphQLConfig.java
@@ -4,31 +4,25 @@ import com.brennaswitzer.cookbook.graphql.support.CachingPreparsedDocumentProvid
 import com.brennaswitzer.cookbook.graphql.support.OffsetConnectionCursorCoercing;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.Scheduler;
-import graphql.ExceptionWhileDataFetching;
 import graphql.ExecutionResult;
 import graphql.execution.AsyncExecutionStrategy;
 import graphql.execution.DataFetcherExceptionHandler;
 import graphql.execution.ExecutionContext;
 import graphql.execution.ExecutionStrategy;
 import graphql.execution.ExecutionStrategyParameters;
-import graphql.execution.ResultPath;
 import graphql.execution.preparsed.PreparsedDocumentProvider;
-import graphql.language.SourceLocation;
 import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLScalarType;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.DataFetcherExceptionResolver;
-import org.springframework.transaction.UnexpectedRollbackException;
-import org.springframework.transaction.support.DefaultTransactionStatus;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.Duration;
 import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Configuration
@@ -89,7 +83,8 @@ public class GraphQLConfig {
     GraphQlSourceBuilderCustomizer sourceBuilderCustomizer(
             Collection<GraphQLScalarType> scalars,
             TransactionTemplate mutationTmpl,
-            ObjectProvider<DataFetcherExceptionResolver> resolvers) {
+            DataFetcherExceptionHandler exceptionHandler) {
+
         // The warning is for IntelliJ's over-aggro application of @NotNull to
         // places where library authors omitted it. In this case, Spring
         // explicitly says the passed definition must be non-null, but leaves
@@ -100,11 +95,15 @@ public class GraphQLConfig {
                 mutationTmpl.getTransactionManager(),
                 mutationTmpl);
 
-        DataFetcherExceptionHandler exceptionHandler =
-                DataFetcherExceptionResolver.createExceptionHandler(resolvers.stream().toList());
-
-        ExecutionStrategy queryStrat = transactionalExecutionStrategy(queryTmpl, exceptionHandler);
-        ExecutionStrategy mutationStrat = transactionalExecutionStrategy(mutationTmpl, exceptionHandler);
+        // Two separate strategy instances are needed, even though they do the
+        // same thing, or the connection pool ends up immediately starved. Not
+        // sure what that's about, but it's consistent, so don't do it.
+        ExecutionStrategy queryStrat = transactionalExecutionStrategy(
+                queryTmpl,
+                exceptionHandler);
+        ExecutionStrategy mutationStrat = transactionalExecutionStrategy(
+                mutationTmpl,
+                exceptionHandler);
 
         return sourceBuilder -> sourceBuilder
                 .configureGraphQl(builder -> builder
@@ -115,6 +114,12 @@ public class GraphQLConfig {
                 .inspectSchemaMappings(report -> log.info("{}", report));
     }
 
+    @Bean
+    DataFetcherExceptionHandler exceptionHandler(
+            List<DataFetcherExceptionResolver> resolvers) {
+        return DataFetcherExceptionResolver.createExceptionHandler(resolvers);
+    }
+
     /*
      This bean is taken from https://blog.akquinet.de/2020/04/16/part-2-graphql-with-spring-boot-jpa-and-kotlin/
      as a way to have "open session in view" behaviour across a GraphQL query.
@@ -122,14 +127,6 @@ public class GraphQLConfig {
      more services which actually do data access, but in this case, our graph
      is actually a single JPA context. Thus, lazy-loading is a core feature, and
      a single transaction per query is necessary.
-
-     The shenanigans w/ global rollback emulate what Spring's
-     AbstractPlatformTransactionManager does if Hibernate has marked the
-     SmartTransaction as rollback-only, rather than the TransactionStatus. Out
-     of the box, such a state will mask the underlying exception(s) to the
-     GraphQL client w/ a generic "silently rolled back" error. By manually
-     propagating the rollback-only state, Spring doesn't do the "smart" check or
-     raise an Exception, because it's now explicit.
     */
     private ExecutionStrategy transactionalExecutionStrategy(
             TransactionTemplate txTmpl,
@@ -139,25 +136,7 @@ public class GraphQLConfig {
             @Override
             public CompletableFuture<ExecutionResult> execute(ExecutionContext executionContext,
                                                               ExecutionStrategyParameters parameters) {
-                return txTmpl.execute(tx -> {
-                    val result = super.execute(executionContext, parameters);
-                    if (tx.isNewTransaction() && tx instanceof DefaultTransactionStatus dtx) {
-                        if (dtx.isGlobalRollbackOnly()) {
-                            tx.setRollbackOnly();
-                            // What Spring would have raised
-                            val ure = new UnexpectedRollbackException(
-                                    "Transaction silently rolled back because it has been marked as rollback-only");
-                            log.error(ure.getMessage(), ure);
-                            // This doesn't actually make it into the response
-                            // for unknown reasons, but does update the context.
-                            executionContext.addError(new ExceptionWhileDataFetching(
-                                    ResultPath.rootPath().segment(""),
-                                    ure,
-                                    new SourceLocation(1, 1)));
-                        }
-                    }
-                    return result;
-                });
+                return txTmpl.execute(tx -> super.execute(executionContext, parameters));
             }
         };
     }
