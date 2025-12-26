73dc664dcafca1f645baf43bc8ca7faa4ae74398
Barney Boisvert <bboisvert@gmail.com>
2025-12-26 10:38:50 -0800

    soak up errors computing diff for recipe history

--- src/main/java/com/brennaswitzer/cookbook/services/DiffService.java
+++ src/main/java/com/brennaswitzer/cookbook/services/DiffService.java
@@ -4,15 +4,31 @@ import com.github.difflib.DiffUtils;
 import com.github.difflib.unifieddiff.UnifiedDiff;
 import com.github.difflib.unifieddiff.UnifiedDiffFile;
 import com.github.difflib.unifieddiff.UnifiedDiffWriter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
 
 @Service
+@Slf4j
 public class DiffService {
 
-    public String diffLinesToPatch(List<String> left, List<String> right) {
+    public String diffLinesToPatch(List<String> left,
+                                   List<String> right) {
+        try {
+            return diffLinesToPatchInternal(left, right);
+        } catch (Exception e) {
+            log.warn(String.format("Failed to diff %s and %s",
+                                   left,
+                                   right),
+                     e);
+            return "";
+        }
+    }
+
+    private String diffLinesToPatchInternal(List<String> left,
+                                            List<String> right) {
         var patch = DiffUtils.diff(left, right);
         var udf = UnifiedDiffFile.from(null, null, patch);
         var ud = UnifiedDiff.from(null, null, udf);
